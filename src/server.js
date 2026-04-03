bd1184d87edf0cfc2938ef48c77b760c7b5c38a6
gnihanyazici <yazicigl@gmail.com>
2026-04-03 21:44:47 +0300

    swagger dokümantasyonu eklendi

--- src/server.js
+++ src/server.js
@@ -2,6 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 import cookieParser from 'cookie-parser';
+import swaggerUi from 'swagger-ui-express'; // Swagger UI eklendi
+import fs from 'fs'; // Dosya okuma modülü eklendi
+import path from 'path';
 
 import contactsRouter from './routers/contacts.js';
 import authRouter from './routers/auth.js';
@@ -10,6 +13,10 @@ import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 export const setupServer = () => {
   const app = express();
+  
+  const swaggerDocument = JSON.parse(
+    fs.readFileSync(path.resolve('docs', 'swagger.json'), 'utf8')
+  );
 
   // 1. Temel Middleware'ler
   app.use(express.json()); // İstek gövdesini (body) JSON olarak okur
@@ -25,6 +32,7 @@ export const setupServer = () => {
     })
   );
 
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
   // 3. Rotaları Bağlama
   app.use('/auth', authRouter); // Kimlik doğrulama işlemleri
   app.use('/contacts', contactsRouter); // İletişim/Rehber işlemleri
